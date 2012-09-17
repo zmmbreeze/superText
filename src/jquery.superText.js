e0291867d269ebc33e9913f79c1e2d0ddf2376c3
mzhou <zmmbreeze0825@gmail.com>
2012-09-17 23:44:39 +0800

    update

--- src/jquery.superText.js
+++ src/jquery.superText.js
@@ -11,5 +11,69 @@
 
 (function($) {
     'use strict';
+    var Tree = {
+        clone: function(node, withChildNode) {
+            if (!withChildNode) {
+                withChildNode = node;
+                node = this;
+            }
 
+            if (!node.isNode) {
+                return null;
+            }
+            if (withChildNode) {
+                var i,l,
+                    newNode = node.clone();
+                for (i=0,l=node.length; i<l; i++) {
+                    if (node[i].isNode) {
+                        newNode.append(node[i].clone(true));
+                    }
+                }
+                return newNode;
+            } else {
+                return Tree.createNode(node.name, node.attr);
+            }
+        },
+        append: function(father, son) {
+            if (!son) {
+                son = father;
+                father = this;
+            }
+            if (!son) {
+                return father;
+            }
+            if (son.parent) {
+                throw 'Node ' + son.name + ' has a parent node!';
+            }
+            father.push(son);
+            son.parent = father;
+            return father;
+        },
+        getDeepestChild: function(node) {
+            var next;
+            while (next = node[node.length-1]) {
+                node = next;
+            }
+            return node;
+        },
+        createNode: function(name, attr) {
+            var n = [];
+            n.isNode = true;
+            n.append = Tree.append;
+            n.clone = Tree.clone;
+            // n.detach = Tree.detach;
+            if (name) {
+                n.name = name;
+            }
+            if (attr) {
+                n.attr = attr;
+            }
+            return n;
+        },
+        createTextNode: function(text) {
+            var textNode = Tree.createNode('#text');
+            textNode.value = text;
+            return textNode;
+        }
+    };
 })(jQuery);
