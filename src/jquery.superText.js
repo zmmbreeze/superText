3ea8de83270a814081a8196b500975e4217633b1
guokr <guokr@zmm.localdomain>
2012-09-17 18:25:17 +0800

    add files

---
+++ src/jquery.superText.js
@@ -0,0 +1,15 @@
+/**
+ * UBBParser
+ * @author mzhou / @zhoumm
+ * @log 0.1 init
+ *
+ */
+
+
+/*jshint undef:true, browser:true, noarg:true, curly:true, regexp:true, newcap:true, trailing:false, noempty:true, regexp:false, strict:true, evil:true, funcscope:true, iterator:true, loopfunc:true, multistr:true, boss:true, eqnull:true, eqeqeq:false, undef:true */
+/*global jQuery:true */
+
+(function($) {
+    'use strict';
+
+})(jQuery);
