c1b98014d0e734546f598ac9fd926a4f74bb4f9a
mzhou <zmmbreeze0825@gmail.com>
2012-09-19 00:04:32 +0800

    update BlockBox/Line

--- src/lib/BlockBox.js
+++ src/lib/BlockBox.js
@@ -1,6 +1,5 @@
 var BlockBox = (function() {
-    var Klass = function() {},
-        blockStyle = {
+    var blockStyle = {
             'block': 1,                             // div/p
             'table': 1,                             // table
             'table-caption': 1,                     // caption
@@ -11,12 +10,70 @@ var BlockBox = (function() {
             'list-item': 1                          // li
         };
 
-    
+    function Klass(element) {
+        this.element = element;
+        // this.lines = [];
+        this.type = 'block';
+    }
+
     Klass.isBlock = function(element) {
         return !!(blockStyle[Util.getComputedStyle(element, 'display')]);
     };
 
-    Klass.prototype.set = function() {
+    Klass.prototype.hasLines = function() {
+        return this.lines.length;
+    };
+
+    Klass.prototype.addLines = function(line) {
+        if (!this.lines) {
+            // init lines
+            this.lines = [];
+        }
+
+        if (line.isLine()) {
+            // single line
+            this.lines.push(line);
+        } else {
+            // array of line
+            // this.lines = this.lines.concat(line);
+            var l,
+                i = 0,
+                l = line.length;
+            for (; i<l; i++) {
+                l = line[i];
+                this.addLines(l);
+            }
+        }
+        return this;
+    };
+
+    Klass.prototype.addBox = function(box) {
+        switch(box.type) {
+        case 'block':
+            if (box.hasLines()) {
+                // combine lines
+                this.addLines(box.lines);
+            } else {
+                // for empty element like: <p></p>
+                // add empty line;
+                this.addLines(Line.createEmptyLine());
+            }
+            break;
+        case 'inline':
+
+            break;
+        default:
+            break;
+        }
+        return this;
+    };
+
+    Klass.prototype.doLayout = function() {
+
+        return this;
+    };
+
+    Klass.prototype.toText = function() {
 
     };
 
--- src/lib/Line.js
+++ src/lib/Line.js
@@ -1,5 +1,26 @@
 var Line = (function() {
-    var Klass = function() {};
+    
+    function Klass() {
+    	// this.inlines = [];
+    	this.isEmpty = true;
+    }
 
+
+    Klass.createEmptyLine = function() {
+    	return new Klass();
+    };
+
+    Klass.prototype.addInline = function(inlineBox) {
+    	if (!this.inlineBoxs) {
+    		this.inlineBoxs = [];
+    	}
+
+
+    	return this;
+    };
+
+    Klass.prototype.toText = function() {
+
+    };
     return Klass;
 })();
